8ec9b831e87cc325003ca230eb6a07b7a350c378
Alok963-ai <alokjangde6@gmail.com>
2025-05-29 08:21:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ photo = "https://i.postimg.cc/dVY9nL63/IMG-20250426-130510-655.jpg"
 cpphoto = "https://i.postimg.cc/dVY9nL63/IMG-20250426-130510-655.jpg"
 appxzip = "https://i.postimg.cc/dVY9nL63/IMG-20250426-130510-655.jpg"
 my_name = "KAKA"
-CHANNEL_ID = "-1002595680027"##change it with your channel 🆔 
+CHANNEL_ID = "-1002624115857"##change it with your channel 🆔 
 
 cookies_file_path = os.getenv("COOKIES_FILE_PATH", "youtube_cookies.txt")
 
